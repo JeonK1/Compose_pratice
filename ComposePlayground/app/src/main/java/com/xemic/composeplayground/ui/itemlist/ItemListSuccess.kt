dc391798f92e05b3a927a0959b76c82d276a3d28
gjeon <gjeon@gmarket.com>
2022-12-06 00:19:01 +0900

    fix : itemList 디자인 오류 수정

--- ComposePlayground/app/src/main/java/com/xemic/composeplayground/ui/itemlist/ItemListSuccess.kt
+++ ComposePlayground/app/src/main/java/com/xemic/composeplayground/ui/itemlist/ItemListSuccess.kt
@@ -1,7 +1,11 @@
 package com.xemic.composeplayground.ui.itemlist
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.xemic.composeplayground.data.model.ItemInfo
 import com.xemic.composeplayground.ui.common.GridList
 
@@ -10,8 +14,14 @@ fun ItemListSuccess(
     modifier: Modifier = Modifier,
     data: List<ItemInfo>,
 ) {
-    GridList(
-        modifier = modifier,
-        data = data
-    )
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp)
+    ) {
+        GridList(
+            modifier = modifier,
+            data = data
+        )
+    }
 }
